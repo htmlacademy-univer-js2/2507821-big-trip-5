e6fad2061c53174b2bf50b840e7458d055512523
MaT0kz <mixail.gerasimov.1994@gmail.com>
2025-05-03 21:03:47 +0500

    Разделяй и властвуй

--- src/presenter/mainPage-presenter.js
+++ src/presenter/mainPage-presenter.js
@@ -1,20 +1,10 @@
-import { render, replace } from '../framework/render';
-import FiltersView from '../view/filters';
-import SortView from '../view/sort';
-import TripEventsListView from '../view/trip-events__list';
-import TripPointView from '../view/trip-point';
-import EditPointFormView from '../view/edit-point';
-import AddNewPointFormView from '../view/add-new-point';
-
-
-const handleEsc = (evt) => {
-  if (evt.key === 'Escape') {
-    return true;
-  } else if (evt.key === 'ArrowUp') {
-    return true;
-  }
-  return false;
-};
+import { render } from '../framework/render';
+import FiltersView from '../view/filters-view';
+import SortView from '../view/sort-view';
+import TripEventsListView from '../view/trip-events__list-view';
+import TripPointView from '../view/trip-point-view';
+import AddNewPointFormView from '../view/add-new-point-view';
+import { handleEsc } from '../utils';
 
 export default class MainPagePresenter {
   filters = new FiltersView();
@@ -30,28 +20,14 @@ export default class MainPagePresenter {
 
   init() {
     this.points = [...this.pointsModel.getPoints()];
-    // Форма редактирвоания + обработчик
 
     render(this.filters, this.filtersContainer);
     render(this.sort, this.contentContainer);
     render(this.tripEventsList, this.contentContainer);
 
-    // Добавление точек и обработчиков событий
     for (let i = 0; i < this.points.length; i++) {
-      const point = new TripPointView({point: this.points[i]});
-      const button = point.element.querySelector('.event__rollup-btn');
-      button.addEventListener('click', () => {
-        const editPointFormView = new EditPointFormView({point: this.points[i]});
-        const editPointForm = editPointFormView.element;
-        editPointForm.addEventListener('submit', () => {});
-        document.addEventListener('keydown', handleEsc);
-        replace(editPointFormView, point);
-        point.removeElement();
-      });
-      render(point, this.tripEventsList.element);
+      const pointView = new TripPointView({point: this.points[i], handleEsc});
+      render(pointView, this.tripEventsList.element);
     }
-
-    // render(this.addNewPointForm, this.tripEventsList.element, RenderPosition.AFTERBEGIN);
-    // render(this.editPointForm, this.tripEventsList.element, RenderPosition.AFTERBEGIN);
   }
 }
